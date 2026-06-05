99f63883ae0c5ce61b1c9c56883a2020acc38780
tiagoolileite <tiago.olileite@gmail.com>
2026-06-05 20:15:13 -0300

    inittial commit

---
+++ src/test/java/br/com/alura/desafios_carreira_java/DesafiosCarreiraJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafios_carreira_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiosCarreiraJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
